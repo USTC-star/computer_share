36039c6c0dc324a09f2c1d39fbd8f83e3630ccd7
unknown <xihxu@ucdavis.edu>
2026-03-15 15:37:15 -0700

    add python

---
+++ python/lens_model.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 15 14:42:47 2026
+
+@author: PinJung
+"""
+"Develope the first temple for MIR lens "
+
+
+
+
+
---
+++ python/my_first_script.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  2 13:50:27 2025
+
+@author: PinJung
+"""
+
+name = "Xinhang"
+print("Hello,", name)
